cdb6ffb55b9fd1272b7682d4dd16564f220701e0
nygmasx <103135929+nygmasx@users.noreply.github.com>
2023-03-01 23:08:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ $('a[href^="#"]').click(function(e) {
     scrollTop: $($(this).attr('href')).offset().top
   }, 500);
 });
+
+// typing animation script
+var typed = new Typed(".typing", {
+strings: ["Découvrez mon profil", "Discover my profile", "Entdecken Sie mein Profil", "Scopri il mio profilo", "Descubre mi perfil", "اكتشف ملف التعريف الخاص بي"],
+typeSpeed: 30,
+backSpeed: 50,
+loop: true
+});
+
